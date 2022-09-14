b250208c74bab929428dae8856dcc0801bad7028
giannis.poulakas <giannis.poulakas@infoteam-software.gr>
2022-09-14 15:33:53 +0300

    small readability improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AngularMaterialModule } from './material.module';
-
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { CreateTaskComponent } from './create-task/create-task.component';
-import { UpdateTaskComponent } from './update-task/update-task.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateTaskComponent,
-    UpdateTaskComponent,
-    TaskListComponent,
-    TaskDetailsComponent
-    
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    AngularMaterialModule,
-    ReactiveFormsModule,
-    FormsModule,
-    FlexLayoutModule
-    
-  ],
-  providers: [AppService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CreateTaskComponent,
+        TaskListComponent,
+        TaskDetailsComponent,
+        UpdateTaskComponent
+
+    ],
+    imports: [
+        AngularMaterialModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        FlexLayoutModule,
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule
+
+    ],
+    providers: [AppService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
@@ -23,45 +24,45 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
     imports: [
         CommonModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatSidenavModule,
         MatBadgeModule,
-        MatListModule,
-        MatGridListModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
-        MatRadioModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatChipsModule,
-        MatTooltipModule,
-        MatTableModule,
-        MatPaginatorModule,
+        MatButtonModule,
         MatCardModule,
-        MatSlideToggleModule
+        MatChipsModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSlideToggleModule,
+        MatTableModule,
+        MatToolbarModule,
+        MatTooltipModule
     ],
     exports: [
-        MatButtonModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatSidenavModule,
         MatBadgeModule,
-        MatListModule,
-        MatGridListModule,
-        MatInputModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        MatRadioModule,
-        MatDatepickerModule,
-        MatChipsModule,
-        MatTooltipModule,
-        MatTableModule,
-        MatPaginatorModule,
+        MatButtonModule,
         MatCardModule,
-        MatSlideToggleModule
+        MatChipsModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatPaginatorModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSlideToggleModule,
+        MatTableModule,
+        MatToolbarModule,
+        MatTooltipModule,
     ],
     providers: [
         MatDatepickerModule,
