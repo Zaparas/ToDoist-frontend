e4be0f7020ebfb3cc5cba519b9be8982b4ab1789
eirinisof <eirinisof@csd.auth.gr>
2022-09-14 15:05:09 +0300

    back ground color depend on priority

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,45 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { Task } from '../model/task'
-import { AppService } from '../app.service'
-import { Router } from '@angular/router'
+import { Component, OnInit } from "@angular/core";
+import { Task } from "../model/task";
+import { AppService } from "../app.service";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-task-list',
-    templateUrl: './task-list.component.html',
-    styleUrls: ['./task-list.component.css']
+  selector: "app-task-list",
+  templateUrl: "./task-list.component.html",
+  styleUrls: ["./task-list.component.css"],
 })
 export class TaskListComponent implements OnInit {
+  tasks?: Task[];
 
-    tasks?: Task[];
+  constructor(private service: AppService, private router: Router) {}
 
-    constructor(
-        private service: AppService,
-        private router: Router
-    ) { }
+  ngOnInit(): void {
+    this.getTasks();
+  }
 
-    ngOnInit(): void {
-        this.getTasks();
+  private getTasks() {
+    this.service.getAllTasks().subscribe((data) => {
+      this.tasks = data;
+    });
+  }
+
+  taskDetails(id: any) {
+    this.router.navigate(["task-details", id]);
+  }
+
+  updateTask(id: any) {
+    this.router.navigate(["update-task", id]);
+  }
+
+  deleteTask(id: any) {
+    this.service.deleteTask(id).subscribe((data) => {
+      console.log(data);
+      this.getTasks();
+    });
+  }
+
+  getColor(priority: any) {
+    2;
+    switch (priority) {
+      case "LOW":
+        return "green";
+      case "NORMAL":
+        return "yellow";
+      case "HIGH":
+        return "orange";
+      case "TOP":
+        return "red";
     }
-
-    private getTasks() {
-        this.service.getAllTasks().subscribe(data => {
-            this.tasks = data;
-        });
-    }
-
-    taskDetails(id: any) {
-        this.router.navigate(['task-details', id]);
-    }
-
-    updateTask(id: any) {
-        this.router.navigate(['update-task', id]);
-    }
-
-    deleteTask(id: any) {
-        this.service.deleteTask(id).subscribe(data => {
-            console.log(data);
-            this.getTasks();
-        })
-    }
-
+    return 0;
+  }
 }
